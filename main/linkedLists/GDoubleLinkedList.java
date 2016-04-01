b82eadf0302b3ff010d5a1f528c6166a010d37dd
Guillermo <gmo.chu92@gmail.com>
2016-04-01 19:55:29 -0400

    DoublyLinkedList impl and test

--- main/linkedLists/GDoubleLinkedList.java
+++ main/linkedLists/GDoubleLinkedList.java
@@ -5,15 +5,61 @@ public class GDoubleLinkedList<E> {
 	private Node first;
 	private int size;
 	
-	private class Node {
-		private E element;
-		private Node next;
-		private Node previous;
+	protected class Node {
+		public E element;
+		public Node next;
+		public Node previous;
 	}
 	
 	public GDoubleLinkedList() {
 		first = null;
 		size = 0;
 	}
+
+	public Node getFirst() {
+		return first;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void insertAtBeginning(E element) {
+		Node newNode = new Node();
+		newNode.element = element;
+		if(isEmpty()){
+			first = newNode;
+		}
+		else {
+			if(size == 1) {
+				first.previous = newNode;
+				first.next = newNode;
+				
+				newNode.next = first;
+				newNode.previous = first;
+				
+				first = newNode;
+			}
+			else {
+				Node beforeFirst = first.previous;
+				 
+				beforeFirst.next = newNode;
+				first.previous = newNode;
+				
+				newNode.next = first;
+				newNode.previous = beforeFirst;
+				
+				first = newNode;
+			}
+		}
+		
+		size++;
+	}
+
+	
 
 }
--- test/linkedLists/GDoubleLinkedListTest.java
+++ test/linkedLists/GDoubleLinkedListTest.java
@@ -1,5 +1,64 @@
 package linkedLists;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
 public class GDoubleLinkedListTest {
+	
+	GDoubleLinkedList<Object> testObject;
+	
+	@Test
+	public void constructorTest() {
+		testObject = new GDoubleLinkedList<>();
+		
+		assertNull(testObject.getFirst());
+		assertEquals(0, testObject.getSize());
+	}
+	
+	@Test
+	public void insertAtBeginningTest_insertFirstElement() {
+		testObject = new GDoubleLinkedList<>();
+		
+		testObject.insertAtBeginning("Guillermo");
+		
+		assertFalse(testObject.isEmpty());
+		assertEquals("Guillermo", testObject.getFirst().element);
+		assertEquals(1, testObject.getSize());
+	}
+	
+	@Test
+	public void insertAtBeginningTest_insertSecondElement() {
+		testObject = new GDoubleLinkedList<>();
+		
+		testObject.insertAtBeginning("Guillermo");
+		testObject.insertAtBeginning("Cecilia");
+		
+		assertEquals("Cecilia", testObject.getFirst().element);
+		assertEquals("Guillermo", testObject.getFirst().next.element);
+		assertEquals(2, testObject.getSize());
+	}
+	
+	@Test
+	public void insertAtBeginningTest_insertMultipleElements() {
+		testObject = new GDoubleLinkedList<>();
+		
+		testObject.insertAtBeginning("Guillermo");
+		testObject.insertAtBeginning("Cecilia");
+		testObject.insertAtBeginning("Elena");
+		testObject.insertAtBeginning("Manuel");
+
+		assertEquals("Manuel", testObject.getFirst().element);
+		assertEquals("Guillermo", testObject.getFirst().previous.element);
+		assertEquals("Elena", testObject.getFirst().next.element);
+		assertEquals(4, testObject.getSize());
+	}
+	
+	@Test
+	public void removeFromBeginningTest() {
+		
+	}
 
 }
