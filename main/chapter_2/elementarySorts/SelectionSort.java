e809727a031fb0274a4be0f62db44dc2993cf9e7
Guillermo <gmo.chu92@gmail.com>
2016-04-11 21:59:57 -0400

    Folder restructure

--- main/chapter_2/elementarySorts/SelectionSort.java
+++ main/chapter_2/elementarySorts/SelectionSort.java
@@ -1,14 +1,15 @@
-package chapter_2;
+package chapter_2.elementarySorts;
 
 import java.util.List;
 
 public class SelectionSort {
 
-	public static List<Integer> sort(List<Integer> toSort) {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static List<Comparable> sort(List<Comparable> toSort) {
 		for(int i = 0; i<toSort.size(); i++) {
 			int smallestIndex = i;
 			for(int j = i+1; j<toSort.size(); j++) {
-				if(toSort.get(smallestIndex) > toSort.get(j)) {
+				if(toSort.get(smallestIndex).compareTo(toSort.get(j)) > 0) {
 					smallestIndex = j;
 				}
 			}
@@ -17,8 +18,9 @@ public class SelectionSort {
 		return toSort;
 	}
 
-	private static void swap(List<Integer> toSort, int i, int smallestIndex) {
-		int temp = toSort.get(i);
+	@SuppressWarnings("rawtypes")
+	private static void swap(List<Comparable> toSort, int i, int smallestIndex) {
+		Comparable temp = toSort.get(i);
 		toSort.set(i, toSort.get(smallestIndex));
 		toSort.set(smallestIndex, temp);
 	}
--- test/chapter_2/elementarySorts/SortTest.java
+++ test/chapter_2/elementarySorts/SortTest.java
@@ -1,4 +1,4 @@
-package chapter_2;
+package chapter_2.elementarySorts;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -8,17 +8,21 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class SelectionSortTest {
+import chapter_2.elementarySorts.SelectionSort;
 
+public class SortTest {
+
+	@SuppressWarnings("rawtypes")
 	@Test
 	public void sortTest() {
-		List<Integer> toSort = Arrays.asList(2, 0, 3, 5, 7, 6, 11, 15, 1);
+		List<Comparable> toSort = Arrays.asList(2, 0, 3, 5, 7, 6, 11, 15, 1);
 		toSort = SelectionSort.sort(toSort);
 		assertEquals(9, toSort.size());
 		assertTrue(isSorted(toSort));
 	}
 	
-	public boolean isSorted(List<Integer> list) {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public boolean isSorted(List<Comparable> list) {
 		for(int i = 1; i<list.size(); i++) {
 			if(list.get(i).compareTo(list.get(i-1)) < 0) {
 				return false;
