4d7cf990f98d9bfb9fb9e9d2ef2a8227e6096c5e
Guillermo <gmo.chu92@gmail.com>
2016-04-12 20:55:25 -0400

    insertion sort

--- main/chapter_2/elementarySorts/InsertionSort.java
+++ main/chapter_2/elementarySorts/InsertionSort.java
@@ -1,5 +1,24 @@
 package chapter_2.elementarySorts;
 
-public class InsertionSort {
+import java.util.List;
 
+/*Compares elements with other elements that are already sorted (left hand side)*/
+public class InsertionSort {
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static List<Comparable> sort(List<Comparable> toSort) {
+		for(int i = 1; i<toSort.size(); i++) {
+			int previousLocation = i;
+			Comparable currentItem = toSort.get(i);
+			for(int j = i-1; j>=0; j--) {
+				if(currentItem.compareTo(toSort.get(j)) < 0) {
+					toSort.add(j, toSort.get(previousLocation));
+					toSort.remove(previousLocation+1);
+					previousLocation = j;
+				}
+			}
+		}
+		return toSort;
+	}
+	
 }
--- test/chapter_2/elementarySorts/GeneralSortTest.java
+++ test/chapter_2/elementarySorts/GeneralSortTest.java
@@ -3,21 +3,37 @@ package chapter_2.elementarySorts;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 
-import chapter_2.elementarySorts.SelectionSort;
-
-public class SortTest {
+public class GeneralSortTest {
 
 	@SuppressWarnings("rawtypes")
 	@Test
-	public void sortTest() {
+	public void selectionSortTest() {
 		List<Comparable> toSort = Arrays.asList(2, 0, 3, 5, 7, 6, 11, 15, 1);
 		toSort = SelectionSort.sort(toSort);
 		assertEquals(9, toSort.size());
+		assertTrue(isSorted(toSort));
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void insertionSortTest() {
+		List<Comparable> toSort = new ArrayList<Comparable>();
+		toSort.add(2);
+		toSort.add(10);
+		toSort.add(1);
+		toSort.add(7);
+		toSort.add(3);
+
+		toSort = InsertionSort.sort(toSort);
+		System.out.println(toSort.toString());
+		
+		assertEquals(5, toSort.size());
 		assertTrue(isSorted(toSort));
 	}
 	
